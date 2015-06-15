280dde04840960807238f5304a1515e1ae931498
dbaumeister <domi@baumis.eu>
2015-06-15 15:22:47 +0200

    Assignment5: - added register comparison operators "Equal" ("==") and "Less Than" ("<") - added tests for those functions in main.cpp

--- main.cpp
+++ main.cpp
@@ -13,6 +13,8 @@ void testSlottedPage();
 void testSPSegment();
 
 void testRegisters();
+void testRegisterEqualComparison();
+void testRegisterLessThanComparison();
 
 std::string intToString(int i) {
     std::stringstream ss;
@@ -24,10 +26,89 @@ std::string intToString(int i) {
 int main(int argc, const char* argv[])
 {
     testRegisters();
-
+    testRegisterEqualComparison();
+    testRegisterLessThanComparison();
     return 0;
 }
 
+
+void testRegisterLessThanComparison(){
+    Register<sizeof(int)> integerRegisterA(INTEGER_REGISTER);
+    Register<sizeof(int)> integerRegisterB(INTEGER_REGISTER);
+
+    for(int i = 0; i < 1000; ++i){
+
+        integerRegisterA.setInteger(i);
+        integerRegisterB.setInteger(i + 1);
+
+        assert(integerRegisterA < integerRegisterB);
+        assert(! (integerRegisterB < integerRegisterA));
+    }
+
+    const unsigned stringsize = 20;
+    Register<stringsize * sizeof(char)> stringRegisterA(STRING_REGISTER);
+    Register<stringsize * sizeof(char)> stringRegisterB(STRING_REGISTER);
+
+    for(int i = 0; i < 8; ++i){ //Keep in mind: "9" > "10" (lexicographic sorting)
+        std::string strA = intToString(i);
+        assert(strA.size() < stringsize);
+
+
+        std::string strB = intToString(i + 1);
+        assert(strB.size() < stringsize);
+
+        stringRegisterA.setString(strA);
+        stringRegisterB.setString(strB);
+
+        assert(stringRegisterA < stringRegisterB);
+        assert(!(stringRegisterB < stringRegisterA));
+    }
+}
+
+void testRegisterEqualComparison(){
+
+    Register<sizeof(int)> integerRegisterA(INTEGER_REGISTER);
+    Register<sizeof(int)> integerRegisterB(INTEGER_REGISTER);
+
+    for(int i = 0; i < 1000; ++i){
+
+        integerRegisterA.setInteger(i);
+        integerRegisterB.setInteger(i);
+
+        assert(integerRegisterA == integerRegisterB);
+        assert(integerRegisterB == integerRegisterA);
+
+
+        integerRegisterB.setInteger(i + 1);
+
+        assert( ! (integerRegisterA == integerRegisterB));
+        assert( ! (integerRegisterB == integerRegisterA));
+    }
+
+    const unsigned stringsize = 20;
+    Register<stringsize * sizeof(char)> stringRegisterA(STRING_REGISTER);
+    Register<stringsize * sizeof(char)> stringRegisterB(STRING_REGISTER);
+
+    for(int i = 0; i < 1000; ++i){
+        std::string str = intToString(i);
+        assert(str.size() < stringsize);
+
+        stringRegisterA.setString(str);
+        stringRegisterB.setString(str);
+
+        assert(stringRegisterA == stringRegisterB);
+        assert(stringRegisterB == stringRegisterA);
+
+
+        std::string strB = intToString(i + 1);
+        assert(strB.size() < stringsize);
+
+        stringRegisterB.setString((strB));
+
+        assert( !(stringRegisterA == stringRegisterB));
+        assert( !(stringRegisterB == stringRegisterA));
+    }
+}
 
 void testRegisters(){
     Register<sizeof(int)> integerRegister(INTEGER_REGISTER);
--- operators/Register.h
+++ operators/Register.h
@@ -9,6 +9,7 @@
 #include <string>
 #include <string.h>
 #include <assert.h>
+#include <iostream>
 
 #define STRING_REGISTER 1
 #define INTEGER_REGISTER 2
@@ -34,7 +35,7 @@ public:
         free(value);
     }
 
-    int getInteger() {
+    int getInteger() const{
         return *(int*)value;
     }
 
@@ -42,7 +43,7 @@ public:
         *(int*)value = intVal;
     }
 
-    std::string getString() {
+    std::string getString() const{
 
         return *(std::string*) value;
     }
@@ -51,8 +52,17 @@ public:
         memcpy(value, &strVal, len);
     }
 
-    unsigned  getType(){
+    unsigned  getType() const{
         return type;
+    }
+
+    int getHashValue(){
+        if(type == STRING_REGISTER){
+            return atoi(getString().c_str()); //TODO evaluate usability of this hash function
+        }
+        else if(type == INTEGER_REGISTER){
+            return getInteger();
+        }
     }
 
 private:
@@ -61,6 +71,30 @@ private:
 
 
 };
+
+
+
+template <unsigned len>
+bool operator ==(const Register<len>& left, const Register<len>& right){
+
+    if(left.getType() == right.getType()){ //same type needed
+        if(left.getType() == STRING_REGISTER) return left.getString() == right.getString();
+        else if(left.getType() == INTEGER_REGISTER) return left.getInteger() == right.getInteger();
+        else return false;
+
+    } else
+        return false;
+
+}
+
+template <unsigned len>
+bool operator <(const Register<len>& left, const Register<len>& right){
+    assert(left.getType() == right.getType()); //same type needed
+
+    if(left.getType() == STRING_REGISTER) return left.getString() < right.getString();
+    else if(left.getType() == INTEGER_REGISTER) return left.getInteger() < right.getInteger();
+    else return false;
+}
 
 
 #endif //PROJECT_REGISTER_H
